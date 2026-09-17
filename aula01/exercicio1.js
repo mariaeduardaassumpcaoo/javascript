45e21ff60a58be1edce22213f38471e167588c71
mariaeduardaassumpcaoo <mariaeduardaassumpcao56@gmail.com>
2026-09-17 16:53:23 -0300

    atualizar

---
+++ aula01/exercicio1.js
@@ -0,0 +1,8 @@
+const qtdPecasPH = 120;
+const horasTurno = 8;
+
+const prodTotal = qtdPecasPH * horasTurno;
+
+console.log(`\nProducao por hora: ${qtdPecasPH} pcs por hora`)
+console.log(`Horas por tudo: ${horasTurno} horas`)
+console.log(`Producao por turno: ${prodTotal} pecas`)
